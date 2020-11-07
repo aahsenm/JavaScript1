7958bfc30d1cc3b0fe9cf1c923e7b93cb5b7077c
Ahsen M <aahsenmedar@gmail.com>
2020-11-07 18:32:43 +0100

    created logHello.js

---
+++ Week1/homework/js-exercises/logHello.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let english = 'Hello World!' ;
+let german= 'Hallo Welt!' ;
+let turkish= 'Merhaba Dunya!';
+let dutch= 'Hallo Wereld!';
+let spanish= 'Olá Mundo!' ;
+let italian= 'Ciao mondo!' ;
+let french= 'Bonjour le monde!' ;
+let turkmen= 'salam dünýä!' ;
+let russian= 'Привет мир!' ;
+let latin= 'salve mundi!' ;
+
+console.log(english, german, turkish, dutch, spanish, italian, french, turkmen, russian, latin)
