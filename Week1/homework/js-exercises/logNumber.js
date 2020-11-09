78497bc41fad73db6c5c5aa879a04c96007cf31d
Ahsen M <aahsenmedar@gmail.com>
2020-11-09 18:52:33 +0100

    Added third exercise which is logNumber.js

---
+++ Week1/homework/js-exercises/logNumber.js
@@ -0,0 +1,7 @@
+'use strict'
+let numberX;
+console.log(the value of x undefined. Because there is no value defined);
+console.log(numberX);
+numberX = 2;
+console.log(I initialized the value and the value of x should be 2 );
+console.log(numberX);
