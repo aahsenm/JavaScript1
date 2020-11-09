43362436f8f2879f94fd44937f9740232e473521
Ahsen M <aahsenmedar@gmail.com>
2020-11-09 19:12:29 +0100

    Added fourth exercise which is logString.js

---
+++ Week1/homework/js-exercises/logString.js
@@ -0,0 +1,7 @@
+'use strict'
+let myString = 'Arzu Ahsen Medar';
+console.log('the value of the myString should be Arzu Ahsen Medar');
+console.log(myString);
+myString =('I like coding');
+console.log('the new value of the myString should be I like coding');
+console.log(myString);
