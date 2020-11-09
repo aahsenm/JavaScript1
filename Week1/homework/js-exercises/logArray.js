a6dc6957331cb45c5dfbc048e1caac68662c5b40
Ahsen M <aahsenmedar@gmail.com>
2020-11-09 22:07:58 +0100

    Added sixth exercise which is logArray.js

---
+++ Week1/homework/js-exercises/logArray.js
@@ -0,0 +1,8 @@
+'use strict'
+let animals = [];
+console.log('the value of animals should be empty array');
+console.log(animals);
+let bestAnimals = ['cat', 'dog', 'rabbit'];
+console.log(bestAnimals);
+bestAnimals.push('Piglet');
+console.log(bestAnimals);
