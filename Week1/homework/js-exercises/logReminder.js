65ccddf4112d0d141d0bdf7dd2a4b58392891e21
Ahsen M <aahsenmedar@gmail.com>
2020-11-09 23:51:26 +0100

    Added ninth exercise which is logReminder.js

---
+++ Week1/homework/js-exercises/logReminder.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let x = 7;
+console.log(x = x % 3);
+/* Result should be 1. Because 3 fits into 7 two times, leaving 1 as the remainder.*/
+
+let y = 21;
+console.log(y = y % 4);
+/* Result should be 1. Because 4 fits into 21 five times, leaving 1 as the remainder.*/
+
+let z = 13;
+console.log(z = z % 2);
+/* Result should be 1. Because 2 fits into 13 six times, leaving 1 as the remainder.*/
